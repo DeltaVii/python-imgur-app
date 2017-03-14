e7cf720afbeee08befea04afc22d6e2146cc37ac
Kelly Norris <samtimmus@gmail.com>
2017-03-14 13:54:13 -0700

    Update 3/14/17
    -Added messaging function that probably(?) works

--- main.py
+++ main.py
@@ -12,11 +12,85 @@ client = pyimgur.Imgur(CLIENT_ID, CLIENT_SECRET)
 class Anon:
     anon_actions = []
     
-    def anon_loop(self):
-        for i in len(range(self.anon_actions)):
-            print(self.anon_actions[i])
-        anon_input = input("Choose from the list above")
+    
+    #Gallery lister
+    anon_actions.append("Gallery Viewer")
+    def anon_gallery(self):
+        #Getting preferences for section
+        section = input("Input 'hot', 'top', or 'user':").lower()
+        #making sure the input works
+        if section != "hot" and section != "top" and section != "user":
+            print("Input not understood, defaulting to 'hot'")
+            section = "hot"
+        #Getting preferences for sorting
+        sort = input("Input 'viral' or 'time':").lower()
+        #making sure the input works
+        if sort != "viral" and sort != "time":
+            print("Input not understood, defaulting to 'viral'")
+            sort = 'viral'
 
+        #creating result variable from preferences
+        result = client.get_gallery(section = section, sort = sort, limit=100)
+        #printing result with number list
+        for i in range(len(result)):
+            print(i+1, ")", end='', sep='')
+            print(result[i].link)
+        #Asking for link opening
+        link_get_bool = input("Want to open any link? y/n:")
+        if link_get_bool == "y":
+            #making a while loop for opening multiple links
+            get_links = True
+            while get_links == True:
+                #Getting input of the number for the link
+                link_get_number = input("Input the number that you want: ")
+                #making input an integer
+                int(link_get_number)
+                #Opening with webbrowser
+                #result list; index number is the input minus 1's link object
+                #.link makes sure to get a link and not the album/image object
+                webbrowser.open(result[int(link_get_number) - 1].link)
+
+                #Asking for a repeat
+                link_get_repeat = input("Would you like to open another? y/n: ")
+                if link_get_repeat == "y".lower():
+                    pass
+                elif link_get_repeat == "n".lower():
+                    get_links = False
+                else:
+                    print("Input not understood, returning to anon_loop")
+                    self.anon_loop()
+            #ending link get loop
+            self.anon_loop()
+            
+        #ending whole loop
+        elif link_get_bool == "n":
+            self.anon_loop()
+        else:
+            self.anon_loop()
+    #### End of Gallery Getter ####
+
+    #Anon loop for anonymous actions
+    def anon_loop(self):
+        #Printing list of actions with numbers
+        for i in range(len(self.anon_actions)):
+            print(i+1,")", end='', sep='')
+            print(self.anon_actions[i])
+        #Getting user input for anon action
+        anon_input = input("Choose from the list above: ")
+        #Gallery  Viewer
+        if anon_input =="Gallery Viewer".lower() or anon_input == "1":
+            self.anon_gallery()
+            
+        #goback option
+        elif anon_input == "back":
+            user_input = input("Anon or Auth?\n").lower()
+            if user_input == "anon":
+                anon.anon_loop
+            elif user_input == "auth":
+                auth.authorization()
+        else:
+            self.anon_loop()
+        
 class Auth:
     #making list of actions
     auth_actions = []
@@ -124,6 +198,21 @@ class Auth:
             else:
                 print("Input not understood")
                 self.get_user()
+
+
+    auth_actions.append("Message User")
+    def message(self):
+        #getting username input
+        username = input("Input username to get:")
+        if username == "back":
+            self.auth_loop()
+        #creates a User object with username
+        user = client.get_user(username)
+        body = input("Input the body of your message: ")
+        subject = input("Input the subject of your message: ")
+
+        user.send_message(body,subject,reply_to=None)
+        self.auth_loop()
     
     def auth_loop(self):
         for i in range(len(self.auth_actions)):
@@ -133,19 +222,20 @@ class Auth:
             self.get_user()
         #go back to the original input
         elif auth_input == "back":
-            user_input = input("Anon or Auth?\n")
-            if user_input == "Anon":
+            user_input = input("Anon or Auth?\n").lower()
+            if user_input == "anon":
                 anon.anon_loop
-            elif user_input == "Auth":
+            elif user_input == "auth":
                 auth.authorization()
         else:
             auth_loop()
-        
+
+#creating instances of the classes
 anon = Anon()
 auth = Auth()
-user_input = input("Anon or Auth?\n")
-if user_input == "Anon":
-    anon.anon_loop
-elif user_input == "Auth":
+user_input = input("Anon or Auth?\n").lower()
+if user_input == "anon":
+    anon.anon_loop()
+elif user_input == "auth":
     auth.authorization()
 
