cf301119b9e65dd4199997092aa0bec6078a6391
Kelly Norris <samtimmus@gmail.com>
2017-04-04 13:04:52 -0700

    Update 4/4/17
    In kivy_testing.py:
    This will be the file to implement the current code until it's caught
    up.
    -Set up title screen using Kivy's screen manager

--- kivy_testing.py
+++ kivy_testing.py
@@ -1,8 +1,42 @@
 from kivy.app import App
 from kivy.uix.button import Button
 
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+
+Builder.load_string("""
+<TitleScreen>:
+    BoxLayout:
+        orientation:'vertical'
+        Image:
+            source:'img/imgurlogo.png'
+            size_hint_y:'.5'
+            
+            
+        BoxLayout:
+            Button:
+                text:'Anonymous functions'
+                    
+            Button:
+                text:'Authorized functions'
+                
+        
+""")
+
+class TitleScreen(Screen):
+    pass
+
+sm = ScreenManager(transition=NoTransition())
+sm.add_widget(TitleScreen(name='title'))
+
 class TestApp(App):
     def build(self):
-        return Button(text="Hello World")
+        return sm
+        
 
-TestApp().run()
+if __name__=="__main__":
+    TestApp().run()
